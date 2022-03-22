fb5087fdd1e3be2988ff627bef9ed8dd48133e7e
charchar <87212793+0xcharchar@users.noreply.github.com>
2022-03-22 22:26:19 +0000

    load ABIs instead of contracts

--- src/ENSSetup.sol
+++ src/ENSSetup.sol
@@ -1,16 +1,39 @@
 // SPDX-License-Identifier: BSD-2-Clause-Patent
 pragma solidity 0.8.10;
 
-import "enscontracts/BaseRegistrarImplementation.sol";
-import "enscontracts/ENSRegistryWithFallback.sol";
-import "enscontracts/PublicResolver.sol";
+interface Cheatcodes {
+  function getCode(string calldata) external returns(bytes memory);
+}
+
+interface PriceOracle {
+  function price () external view returns(uint); // TODO
+}
+
+interface IETHRegistrarController {
+  function rentPrice (string memory, uint) external view returns(uint);
+  function valid (string memory) external pure returns(bool);
+  function available (string memory) external view returns(bool);
+  function makeCommitment (string memory, address, bytes32) external pure returns(bytes32);
+  function makeCommitmentWithConfig (string memory, address, bytes32, address, address) external pure returns(bytes32);
+  function commit (bytes32) external;
+  function register (string calldata, address, uint, bytes32) external payable;
+  function registerWithConfig (string memory, address, uint, bytes32, address, address) external payable;
+  function renew (string calldata, uint) external payable;
+  function supportsInterface (bytes3) external pure returns(bool);
+
+  // onlyOwner functions
+  function setPriceOracle (PriceOracle) external;
+  function setCommitmentAges (uint, uint) external;
+  function withdraw () external;
+}
 
 contract ENSSetup {
-  ENSRegistryWithFallback public registry;
-  BaseRegistrarImplementation public registrar;
-  PublicResolver public resolver;
+  Cheatcodes public cheats;
 
-  function deploy () {
-    registry = new ENSRegistryWithFallback();
+  constructor (Cheatcodes _cheats) {
+    cheats = _cheats;
+  }
+
+  function deploy () public {
   }
 }
--- src/test/ENSSetup.t.sol
+++ src/test/ENSSetup.t.sol
@@ -2,17 +2,13 @@
 pragma solidity 0.8.10;
 
 import "ds-test/test.sol";
-import "../ENSSetup.sol";
 
 contract ENSSetupTest is DSTest {
-  ENSSetup public ens = new ENSSetup();
 
   function setUp() public {
-    ens.deploy();
   }
 
   function testExample() public {
-    log_named_address("registry addr", address(ens.registry()));
     assertTrue(true);
   }
 }
