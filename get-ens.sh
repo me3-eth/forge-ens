a69e4dcacc3360bdc19067dba757d1e6728537f5
charchar <87212793+0xcharchar@users.noreply.github.com>
2022-03-22 17:42:48 +0000

    feat: pull bytecode and abi, merge into single json file
    This file should now be the format used by forge cheatcodes to load the
    ENS contracts

--- get-ens.sh
+++ get-ens.sh
@@ -2,12 +2,22 @@
 
 export $(grep -v '^#' .env | xargs)
 
+# Generate list of ENS related addresses, env var must start with ENS_ADDR_
 addrs=( $(compgen -A variable | grep ENS_ADDR_) )
-endpoint=https://api.etherscan.io/
+etherscanHost=https://api.etherscan.io
+rpcHost=https://eth-mainnet.alchemyapi.io/v2
 
 for i in "${addrs[@]}"
 do
-  content=$(curl "${endpoint}/api?module=contract&action=getsourcecode&address=${!i}&apikey=${ETHERSCAN_API_KEY}")
-  filename=$(jq -r '.result[0].ContractName' <<< "${content}").sol
-  jq -r '.result[0].SourceCode' <<< "${content}" > src/$filename
+  # Download contract name and abi from Etherscan
+  etherscanContent=$(curl "${etherscanHost}/api?module=contract&action=getsourcecode&address=${!i}&apikey=${ETHERSCAN_API_KEY}")
+  filename=$(jq -r '.result[0].ContractName' <<< "${etherscanContent}").json
+  abi=$(jq -r '.result[0].ABI' <<< "${etherscanContent}")
+
+  # Download bytecode from RPC
+  rpcReq=$(printf "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getCode\",\"params\":[\"%s\",\"latest\"],\"id\":0}" "${!i}")
+  rpcContent=$(curl "${rpcHost}/${ALCHEMY_API_KEY}" -XPOST -H "Content-Type: application/json" -d "${rpcReq}")
+  bytecode=$(jq -r '.result' <<< "${rpcContent}")
+
+  printf "{\"abi\":%s,\"bytecode\":\"%s\"}" "${abi}" "${bytecode}" > abis/$filename
 done
